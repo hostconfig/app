fec9aba68a461ad6924bfec154b16d187ea2823d
StoneyDSP <nathanjhood@googlemail.com>
2023-12-22 01:36:36 +0100

    Fixed types
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,10 @@
-import {
-  type Application
-} from 'express'
-
-declare module '@hostconfig/app' {
-  const app: Application
-}
+#!/usr/bin/env node
+/**
+ * Module dependencies.
+ */
+import { Application } from 'express';
+import debugLib from 'debug';
+declare const app: Application;
+export declare const debug: debugLib.Debugger;
+export default app;
+//# sourceMappingURL=index.d.ts.map
